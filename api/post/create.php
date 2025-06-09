08cf1f84a7fd75e72ae39342c0e707938abf359e
NPC <pichetfuengfoo@gmail.com>
2025-06-10 00:27:41 +0700

    cleanup post

--- api/post/create.php
+++ api/post/create.php
@@ -1,40 +1,39 @@
 <?php
 
-	// Headers
-	header('Access-Control-Allow-Origin: *');
-	header('Content-Type: application/json');
-	header('Access-Control-Allow-Methods: POST');
-	header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-	// Includes
-	include_once '../../config/Database.php';
-	include_once '../../models/Post.php';
+// Includes
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
 
-	// DB Connect
-	$database = new Database();
-	$db = $database->connect();
+// DB Connect
+$database = new Database();
+$db = $database->connect();
 
-	// Post Object
-	$post = new Post($db);
+// Post Object
+$post = new Post($db);
 
-	// Get Raw Posted Data
-	$data = json_decode(file_get_contents('php://input'));
+// Get Raw Posted Data
+$data = json_decode(file_get_contents('php://input'));
 
-	// Create Post
-	if (isset($data->title) && isset($data->body) && isset($data->author) && isset($data->category_id)) {
-		$post->title = $data->title;
-		$post->body = $data->body;
-		$post->author = $data->author;
-		$post->category_id = $data->category_id;
+// Create Post
+if (isset($data->title) && isset($data->body) && isset($data->author) && isset($data->category_id)) {
+	$post->title = $data->title;
+	$post->body = $data->body;
+	$post->author = $data->author;
+	$post->category_id = $data->category_id;
 
-		if ($post->create()) {
-			echo json_encode([
-				'message' => 'Post Created'
-			]);
-		}
-	}
-	else {
+	if ($post->create()) {
 		echo json_encode([
-			'message' => 'Post Not Created'
+			'message' => 'Post Created'
 		]);
 	}
+} else {
+	echo json_encode([
+		'message' => 'Post Not Created'
+	]);
+}
--- api/post/read.php
+++ api/post/read.php
@@ -1,49 +1,48 @@
 <?php
 
-	// Headers
-	header('Access-Control-Allow-Origin: *');
-	header('Content-Type: application/json');
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
-	// Includes
-	include_once '../../config/Database.php';
-	include_once '../../models/Post.php';
+// Includes
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
 
-	// DB Connect
-	$database = new Database();
-	$db = $database->connect();
+// DB Connect
+$database = new Database();
+$db = $database->connect();
 
-	// Post Object
-	$post = new Post($db);
+// Post Object
+$post = new Post($db);
 
-	// Post Query
-	$result = $post->read();
+// Post Query
+$result = $post->read();
 
-	// Get Row Count
-	$num = $result->rowCount();
-	if ($num > 0) {
-		$posts = array();
-		$posts['data'] = array();
+// Get Row Count
+$num = $result->rowCount();
+if ($num > 0) {
+	$posts = array();
+	$posts['data'] = array();
 
-		while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-			extract($row);
+	while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+		extract($row);
 
-			$post = array(
-				'id' => $id,
-				'title' => $title,
-				'body' => html_entity_decode($body),
-				'author' => $author,
-				'category_id' => $category_id,
-				'category_name' => $category_name
-			);
+		$post = array(
+			'id' => $id,
+			'title' => $title,
+			'body' => html_entity_decode($body),
+			'author' => $author,
+			'category_id' => $category_id,
+			'category_name' => $category_name
+		);
 
-			array_push($posts['data'], $post);
-		}
-		// Turn to JSON
-		echo json_encode($posts);
+		array_push($posts['data'], $post);
 	}
-	else {
-		// No Posts
-		echo json_encode([
-			'message' => 'No Posts Found'
-		]);
-	}
+	// Turn to JSON
+	echo json_encode($posts);
+} else {
+	// No Posts
+	echo json_encode([
+		'message' => 'No Posts Found'
+	]);
+}
