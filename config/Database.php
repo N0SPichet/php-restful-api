485c8bd799c8733f3b0f8935d5516fea76d15b15
NPC <pichetfuengfoo@gmail.com>
2025-06-09 15:57:17 +0700

    test

--- config/Database.php
+++ config/Database.php
@@ -13,14 +13,15 @@ class Database
 	private $db_name = 'my_blog';
 	private $conn;
 
-	public function connect() {
+	public function connect()
+	{
 		$this->conn = null;
 
 		try {
-			$this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name, $this->username, $this->password);
+			$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $e) {
-			echo 'Connection Error: '.$e->getMessage();
+			echo 'Connection Error: ' . $e->getMessage();
 		}
 
 		return $this->conn;
