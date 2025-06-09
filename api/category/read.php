4bb2eb8c5eea044fc0f1a15493eec903f8745802
NPC <pichetfuengfoo@gmail.com>
2025-06-10 00:27:03 +0700

    cleanup category

--- api/category/read.php
+++ api/category/read.php
@@ -1,45 +1,44 @@
 <?php
 
-	// Headers
-	header('Access-Control-Allow-Origin: *');
-	header('Content-Type: application/json');
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
-	// Includes
-	include_once '../../config/Database.php';
-	include_once '../../models/Category.php';
+// Includes
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
 
-	// DB Connect
-	$database = new Database();
-	$db = $database->connect();
+// DB Connect
+$database = new Database();
+$db = $database->connect();
 
-	// Category Object
-	$post = new Category($db);
+// Category Object
+$post = new Category($db);
 
-	// Category Query
-	$result = $post->read();
+// Category Query
+$result = $post->read();
 
-	// Get Row Count
-	$num = $result->rowCount();
-	if ($num > 0) {
-		$posts = array();
-		$posts['data'] = array();
+// Get Row Count
+$num = $result->rowCount();
+if ($num > 0) {
+	$posts = array();
+	$posts['data'] = array();
 
-		while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-			extract($row);
+	while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+		extract($row);
 
-			$post = array(
-				'id' => $id,
-				'name' => $name
-			);
+		$post = array(
+			'id' => $id,
+			'name' => $name
+		);
 
-			array_push($posts['data'], $post);
-		}
-		// Turn to JSON
-		echo json_encode($posts);
+		array_push($posts['data'], $post);
 	}
-	else {
-		// No Categorys
-		echo json_encode([
-			'message' => 'No Categorys Found'
-		]);
-	}
+	// Turn to JSON
+	echo json_encode($posts);
+} else {
+	// No Categorys
+	echo json_encode([
+		'message' => 'No Categorys Found'
+	]);
+}
